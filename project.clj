bb3a682686b2d076cc17eb107e43167c8ef1393d
Philip Diaz <lambdahands@gmail.com>
2017-09-22 14:13:43 -0500

    Setup project.clj, license, CoC

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject opticlj "1.0.0-alpha1"
+  :description "A Clojure expectation/snapshot testing library, inspired by cram, ppx_expect, and jest"
+  :url "http://github.com/lambdahands/opticlj"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/opticlj/core_test.clj
@@ -0,0 +1,7 @@
+(ns opticlj.core-test
+  (:require [clojure.test :refer :all]
+            [opticlj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
