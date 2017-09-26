e2c90430dad29b42199775b75b2372798ace1d3e
Philip Diaz <lambdahands@gmail.com>
2017-09-26 18:49:28 -0500

    Remove SNAPSHOT, prepare release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject opticlj "1.0.0-alpha3-SNAPSHOT"
+(defproject opticlj "1.0.0-alpha3"
   :description "A Clojure expectation/snapshot testing library, inspired by cram, ppx_expect, and jest"
   :url "http://github.com/lambdahands/opticlj"
   :license {:name "MIT"}
